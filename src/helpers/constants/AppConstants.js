fac42f5eb93a48d7a3d8e26cef952573ff85c653
Sheikh Abdul Basit <sheikh.abdul.basit95@gmail.com>
2024-04-25 16:42:20 +0530

    feat(): updated constant resources

--- src/helpers/constants/AppConstants.js
+++ src/helpers/constants/AppConstants.js
@@ -1,3 +1,9 @@
+import InstaIcon from "src/assets/images/logos/InstaIcon.png";
+import FacebookIcon from "src/assets/images/logos/FBIcon.png";
+import LinkedinIcon from "src/assets/images/logos/LinkedinIcon.png";
+import TwitterIcon from "src/assets/images/logos/TwitterIcon.png";
+import YoutubeIcon from "src/assets/images/logos/YoutubeIcon.png";
+
 export const InitialNavOptions = [
   { id: 1, label: "Storie" },
   { id: 2, label: "Loev" },
@@ -58,4 +64,27 @@ export const ColorOptions = [
     label: "cream",
     color: "#F5D266",
   },
+];
+
+const Models = ["Storie", "One", "Loev"];
+
+const Company = ["About", "Blog"];
+
+const QuickLinks1 = ["Become a Dealer", "Locate a Dealer", "Emagine"];
+
+const QuickLinks2 = ["Book a Test Ride", "Book Now", "Contact"];
+
+export const Links = [
+  { label: "Models", items: Models },
+  { label: "Company", items: Company },
+  { label: "Quick Links", items: QuickLinks1 },
+  { label: "  ", items: QuickLinks2 },
+];
+
+export const Icons = [
+  { id: "icon1", imgSrc: InstaIcon },
+  { id: "icon2", imgSrc: FacebookIcon },
+  { id: "icon3", imgSrc: TwitterIcon },
+  { id: "icon4", imgSrc: LinkedinIcon },
+  { id: "icon5", imgSrc: YoutubeIcon },
 ];
