1c9356a8adc6525187a83964d2d918834a37d38f
Sheikh Abdul Basit <sheikh.abdul.basit95@gmail.com>
2024-04-25 15:37:21 +0530

    feat(): adds constants file

---
+++ src/helpers/constants/AppConstants.js
@@ -0,0 +1,61 @@
+export const InitialNavOptions = [
+  { id: 1, label: "Storie" },
+  { id: 2, label: "Loev" },
+  { id: 3, label: "One" },
+  { id: 4, label: "Superbike", isNew: true },
+];
+
+export const AdditionalNavOptions = [
+  { id: 7, label: "Become a Dealer" },
+  { id: 6, label: "Book a Test Ride" },
+  { id: 5, label: "Book Now" },
+];
+
+export const Steppers = [
+  { id: 8, label: "01 Select Model" },
+  { id: 9, label: "02 Select Color" },
+  { id: 10, label: "03 Your Details and Your Preferred Location" },
+];
+
+export const ColorOptions = [
+  {
+    id: "C1",
+    label: "aqua",
+    color: "#A0CABB",
+  },
+  {
+    id: "C2",
+    label: "grey",
+    color: "#707485",
+  },
+  {
+    id: "C3",
+    label: "green",
+    color: "#233A78",
+  },
+  {
+    id: "C4",
+    label: "lightaqua",
+    color: "#93D4DE",
+  },
+  {
+    id: "C8",
+    label: "greenaqua",
+    color: "#C5DBF4",
+  },
+  {
+    id: "C5",
+    label: "orange-red",
+    color: "#B42025",
+  },
+  {
+    id: "C6",
+    label: "white",
+    color: "#fff",
+  },
+  {
+    id: "C7",
+    label: "cream",
+    color: "#F5D266",
+  },
+];
